66d1fe962f3ac05a825b4341fb6a65fb8a199746
eldarhadzs33 <eldarbus33@gmail.com>
2025-05-12 20:28:30 +0200

    canonical mode vs raw mode

--- kilo.c
+++ kilo.c
@@ -1,5 +1,10 @@
-int main()
-{
-  return 1;
+#include <stdio.h>
+#include <unistd.h>
+int main(){
+  int i=0;
+  char c;
+  while (read(STDIN_FILENO,&c ,1 )==1 && c!='q') {
+      i++; 
+  }
+  printf("You just typed %d characters", i);
 }
-
